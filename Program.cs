8033e92e21299ab661f6f13cc0000353ea698e94
SamhithThatikonda <st1106@scarletmail.rutgers.edu>
2024-07-26 12:41:44 -0400

    Provided with Configuration of DB Provider and Connection String which will be used by ApplicationDbContext in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using WebApplication1.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// We are injecting DbContextOptions<ApplicationDbContext> into the ApplicationDbContext constructor, which is a class that inherits from DbContextOptions, and it is used to configure the DbContext.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+// We are configuring the database provider and connection string that will be used by the ApplicationDbContext.
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext")));
 
 var app = builder.Build();
 
