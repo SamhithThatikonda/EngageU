69c4cd31813d07669029234f7ca9c61548a90fa6
SamhithThatikonda <st1106@scarletmail.rutgers.edu>
2024-07-26 12:40:28 -0400

    Provided with Configuration of DB Provider and Connection String which will be used by ApplicationDbContext in Program.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,11 +2,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Data
 {
+    // ApplicationDbContext is a class that inherits from DbContext, which is a class provided by Entity Framework Core, and applicationDbContext acts as a bridge between our entities and the database.
+    // DbContext is a class that represents a session with the database and allows us to query and save instances of our entities.
+    // ApplicationDbContext is a class that represents a session with the database and allows us to query and save instances of our entities.
     public class ApplicationDbContext : DbContext
     {
-        public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
         }
+        // DBSet is a collection of entities that can be queried from the database, and it includes methods for filtering, sorting, and more.
+        public DbSet<Student> MyProperty { get; set; }
+        
     }
 }
---
+++ Models/Entities/Student.cs
@@ -0,0 +1,11 @@
+namespace WebApplication1.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool SubscribedToNewsletter { get; set; }
+    }
+}
